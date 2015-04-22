1a8755bd9b9d56c6042bb1865feca9cd1e1773dd
Kaija <kaija.chang@gmail.com>
2015-04-22 09:49:05 +0800

    add imported test and add destroyAll function

---
+++ test/imported.test.js
@@ -0,0 +1,7 @@
+describe('elasticsearch imported features', function() {
+  before(function() {
+    require('./init.js');
+  });
+  require('loopback-datasource-juggler/test/common.batch.js');
+  require('loopback-datasource-juggler/test/include.test.js');
+});
--- test/init.js
+++ test/init.js
@@ -1,10 +1,12 @@
 module.exports = require('should');
-var chai = require('chai');
-global.expect = chai.expect;
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = require('rc')('loopback', {test: {elasticsearch:{}}}).test.elasticsearch;
+
+var es = require('../lib/elasticsearch');
+var chai = require('chai');
+global.expect = chai.expect;
 
 if (process.env.CI) {
 	config = {
@@ -14,7 +16,6 @@ if (process.env.CI) {
 		type: 'demo'
 	};
 }
-var es = require('../lib/elasticsearch');
 
 global.getDataSource = global.getSchema = function(customConfig) {
 	var db = new DataSource(require('../'), customConfig || config);
